84c78204bb4dc92906d8c42454319c21a46b58e2
Slava Knyazev <slava@knyz.org>
2016-03-03 23:52:55 -0500

    Added paste image module

--- files/app.js
+++ files/app.js
@@ -445,6 +445,35 @@ $(window).resize(function() {
   isScrolledToBottom = true;
   out.scrollTop = out.scrollHeight - out.clientHeight;
 });
+//PAste Image Module
+document.getElementById('#m').onpaste = function (event) {
+  // use event.originalEvent.clipboard for newer chrome versions
+  var items = (event.clipboardData  || event.originalEvent.clipboardData).items;
+  console.log(JSON.stringify(items)); // will give you the mime types
+  // find pasted image among pasted items
+  var blob = null;
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].type.indexOf("image") === 0) {
+      blob = items[i].getAsFile();
+    }
+  }
+  // load image if there is a pasted image
+  if (blob !== null) {
+    var reader = new FileReader();
+    reader.onload = function(event) {
+      console.log(event.target.result); // data url!
+      var imageobj = {
+        user: uservar.usr,
+        key: uservar.key,
+        data: event.target.result
+      };
+
+      socket.emit("image", imageobj);
+    };
+    reader.readAsDataURL(blob);
+  }
+}
+
 //Make everything functional
 $(document).ready(function() {
   //First of all we NEED to authenticate or things will be breaking :/
